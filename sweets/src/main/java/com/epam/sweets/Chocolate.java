81ef5adb0542b8cba34c65468bfc5801d8b580eb
ravitejakolla04 <ravitejakolla04@gmail.com>
2020-03-07 00:17:50 +0530

    Initial Commit

---
+++ sweets/src/main/java/com/epam/sweets/Chocolate.java
@@ -0,0 +1,11 @@
+package com.epam.sweets;
+
+public class Chocolate extends Sweets{
+	String Name;
+	int cost;
+	float weight;
+	int sweetness;
+	public Chocolate(String Name,int cost,float weight,int sweetness) {
+		super(Name,cost,weight,sweetness);
+	}
+}
---
+++ sweets/src/main/java/com/epam/sweets/Jelly.java
@@ -0,0 +1,11 @@
+package com.epam.sweets;
+
+public class Jelly extends Sweets{
+	String Name;
+	int cost;
+	float weight;
+	int sweetness;
+	public Jelly(String Name,int cost,float weight,int sweetness) {
+		super(Name,cost,weight,sweetness);
+	}
+}
